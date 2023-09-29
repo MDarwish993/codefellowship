1ffa2ca20dc3acba15ab4ec8f932902f1e4a2d39
MDarwish993 <m.darwish993@hotmail.com>
2023-09-29 16:33:58 +0300

    lab18Solution

--- src/main/java/com/mohammadDarwish/codefellowship/repository/PostRepository.java
+++ src/main/java/com/mohammadDarwish/codefellowship/repository/PostRepository.java
@@ -1,7 +1,12 @@
 package com.mohammadDarwish.codefellowship.repository;
 
+import com.mohammadDarwish.codefellowship.models.ApplicationUser;
 import com.mohammadDarwish.codefellowship.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Set;
+
 public interface PostRepository extends JpaRepository<Post,Long> {
+    List<Post> findAllByApplicationUserIn(Set<ApplicationUser> followed);
 }
